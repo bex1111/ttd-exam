b5f7fe965ccd0cc3c2dd9b6f41adcfe3ff2564e6
Barák Barna <barna.barak@idomsoft.hu>
2020-09-24 09:54:40 +0200

    Refact

--- src/main/java/hu/idomsoft/ttdexam/MachineRecognizer.java
+++ src/main/java/hu/idomsoft/ttdexam/MachineRecognizer.java
@@ -1,6 +1,7 @@
 package hu.idomsoft.ttdexam;
 
 public class MachineRecognizer {
+
     public String recognize(String ticketCode) {
         if (ticketCode.contains("xxx")) {
             return "M";
--- src/test/java/hu/idomsoft/ttdexam/MachineRecognizerTest.java
+++ src/test/java/hu/idomsoft/ttdexam/MachineRecognizerTest.java
@@ -1,25 +1,30 @@
 package hu.idomsoft.ttdexam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MachineRecognizerTest {
 
+    private MachineRecognizer machineRecognizer;
+
+    @Before
+    public void setUp() {
+        machineRecognizer = new MachineRecognizer();
+    }
+
     @Test
     public void metroTest() {
-        MachineRecognizer machineRecognizer = new MachineRecognizer();
         Assert.assertEquals("M", machineRecognizer.recognize("0643xxx911281305"));
     }
 
     @Test
     public void nightlyBusTest() {
-        MachineRecognizer machineRecognizer = new MachineRecognizer();
         Assert.assertEquals("N", machineRecognizer.recognize("91451312040956"));
     }
 
     @Test
     public void otherMachineTest() {
-        MachineRecognizer machineRecognizer = new MachineRecognizer();
         Assert.assertEquals("O", machineRecognizer.recognize("5151312040956"));
         Assert.assertEquals("O", machineRecognizer.recognize("58151312040956"));
     }
