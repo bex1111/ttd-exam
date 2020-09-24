a24c514a600d8aa541912fced468798e9bf19e5d
Barák Barna <barna.barak@idomsoft.hu>
2020-09-24 11:31:32 +0200

    reafct and TDD 14

--- src/test/java/hu/idomsoft/ttdexam/TicketValidatorTest.java
+++ src/test/java/hu/idomsoft/ttdexam/TicketValidatorTest.java
@@ -61,7 +61,13 @@ public class TicketValidatorTest {
 
     @Test
     public void validOtherLineNumberTicketTest() {
-        LocalDateTime testDate = LocalDateTime.parse("2019-11-28 01:05", TEST_DATE_TIME_FORMATTER);
-        Assert.assertTrue(ticketValidator.isValid(testDate, "1451311280305", 14));
+        LocalDateTime testDate = LocalDateTime.parse("2019-11-28 13:05", TEST_DATE_TIME_FORMATTER);
+        Assert.assertTrue(ticketValidator.isValid(testDate, "1451311281605", 14));
+    }
+
+    @Test
+    public void invalidOtherLineNumberTicketTest() {
+        LocalDateTime testDate = LocalDateTime.parse("2019-11-28 13:05", TEST_DATE_TIME_FORMATTER);
+        Assert.assertFalse(ticketValidator.isValid(testDate, "11451311281305", 14));
     }
 }
