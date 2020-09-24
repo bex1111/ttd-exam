69c0cc7dc9b067491bbf3c878efe645ba1fa3e65
Barák Barna <barna.barak@idomsoft.hu>
2020-09-24 10:55:35 +0200

    TDD 7

--- src/main/java/hu/idomsoft/ttdexam/TicketValidator.java
+++ src/main/java/hu/idomsoft/ttdexam/TicketValidator.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 public class TicketValidator {
 
     private static final int METRO_TRAVEL_TIME = 80;
+    public static final int NIGHTLY_TRAVEL_TIME = 120;
 
-    public boolean validate(LocalDateTime validationDate, String ticketCode) {
-        return !new TicketTimeCalculator().calculate(ticketCode).minusMinutes(METRO_TRAVEL_TIME).isAfter(validationDate);
+    public boolean isValid(LocalDateTime validationDate, String ticketCode) {
+        if (new MachineRecognizer().recognize(ticketCode).equals("M")) {
+            return !new TicketTimeCalculator().calculate(ticketCode).minusMinutes(METRO_TRAVEL_TIME).isAfter(validationDate);
+        }
+        return !new TicketTimeCalculator().calculate(ticketCode).minusMinutes(NIGHTLY_TRAVEL_TIME).isAfter(validationDate.withYear(LocalDateTime.now().getYear()));
     }
 }
--- src/test/java/hu/idomsoft/ttdexam/TicketValidatorTest.java
+++ src/test/java/hu/idomsoft/ttdexam/TicketValidatorTest.java
@@ -1,6 +1,7 @@
 package hu.idomsoft.ttdexam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -9,18 +10,30 @@ import java.time.format.DateTimeFormatter;
 public class TicketValidatorTest {
 
     private static final DateTimeFormatter TEST_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private TicketValidator ticketValidator;
+
+    @Before
+    public void setUp() {
+        ticketValidator = new TicketValidator();
+    }
 
     @Test
     public void validMetroTicket() {
         LocalDateTime testDate = LocalDateTime.parse("2019-11-28 13:05", TEST_DATE_TIME_FORMATTER);
-        Assert.assertTrue(new TicketValidator().validate(testDate, "0643xxx911281425"));
+        Assert.assertTrue(ticketValidator.isValid(testDate, "0643xxx911281425"));
     }
 
     @Test
     public void invalidMetroTicket() {
         LocalDateTime testDate = LocalDateTime.parse("2019-11-28 13:05", TEST_DATE_TIME_FORMATTER);
-        Assert.assertFalse(new TicketValidator().validate(testDate, "0643xxx911281426"));
+        Assert.assertFalse(ticketValidator.isValid(testDate, "0643xxx911281426"));
     }
 
+    @Test
+    public void validNightlyTicket() {
+        LocalDateTime testDate = LocalDateTime.parse("2019-11-28 01:05", TEST_DATE_TIME_FORMATTER);
+        Assert.assertTrue(ticketValidator.isValid(testDate, "91451311280305"));
     }
+
+
 }
